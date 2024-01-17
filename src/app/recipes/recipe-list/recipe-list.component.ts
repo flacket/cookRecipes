8f385a3c7aa2df0b2277ae5831765a5800788d68
flacket <facundoggallardo@gmail.com>
2024-01-17 14:17:44 -0300

    Layout Recipe Page

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RecipeItemComponent } from './recipe-item/recipe-item.component';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,5 +10,17 @@ import { RecipeItemComponent } from './recipe-item/recipe-item.component';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Cheesecake',
+      'Un pastel de queso o tarta de queso es un postre muy popular desde el siglo XX.',
+      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F21%2F2015%2F02%2F11%2Fcheesecake-facil-con-leche-condensada-2000.jpg'
+    ),
+    new Recipe(
+      'Galletas de Avena',
+      'Son un tipo de galleta elaborado en base a la harina de trigo con el cereal de avena.',
+      'https://mojo.generalmills.com/api/public/content/DghZOSz42kCwArruP2Z_IA_gmi_hi_res_jpeg.jpeg'
+    )
+  ];
 
 }
