3ae0d6cd8e37fd49ecb954093f1942d89961bec2
flacket <facundoggallardo@gmail.com>
2024-02-07 00:26:07 -0300

    Shopping-Edit: addIngredient function added

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,19 +1,30 @@
 import { Component } from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './shopping-edit.component.html',
   styleUrl: './shopping-edit.component.css',
 })
 export class ShoppingEditComponent {
+  newIngredient = new FormGroup({
+    name: new FormControl('', Validators.required),
+    amount: new FormControl('', Validators.required)
+  });
 
-  constructor(private ShoppingListService: ShoppingListService) {}
+  constructor(
+    private ShoppingListService: ShoppingListService
+  ) {}
 
-  onAddIngredient (name:string, amount:string) {
-    this.ShoppingListService.addIngredient(new Ingredient(name, Number(amount)));
+  onSubmit () {
+    console.log(this.newIngredient.value);
+    alert(`Nombre: ${this.newIngredient.value.name}
+Cantidad: ${this.newIngredient.value.amount}`);
+    //let item = new Ingredient(this.newIngredient.value.name, this.newIngredient.value.amount);
+    //this.ShoppingListService.addIngredient(item);
   }
 }
